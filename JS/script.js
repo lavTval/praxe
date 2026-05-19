cef2a03a33e24eb695cca001a46b5efbfdef04ac
filip <gemela.filip@gmail.com>
2026-05-19 13:12:08 +0200

    JS

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,39 @@
-alert("Idk");
+console.log("Filip Gemela");
+console.log("Pizza");
+const var1 = 1;
+const var2 = 5;
+console.log(var1 + var2);
+console.log(var1 - var2);
+console.log(var1 * var2);
+console.log(var1 / var2);
+const name = prompt("Name");
+const color = prompt("Color");
+alert(`Hi ${name}! Your favorite color is ${color}`);
+const age = prompt("Age");
+if (Number(age) < 13) {
+  alert("You are a child");
+} else if (Number(age) < 20) {
+  alert("You are a teenager");
+} else {
+  alert("You are an adult");
+}
+
+const text = document.getElementById("text");
+const button = document.getElementById("button");
+const loading = document.getElementById("loading");
+
+button.addEventListener("click", async () => {
+  loading.style.display = "flex";
+  await fetchMessage();
+  loading.style.display = "none";
+  text.innerText = "Hello from the future";
+  text.style.color = "blue";
+});
+
+function fetchMessage() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 2000)
+  });
+}
